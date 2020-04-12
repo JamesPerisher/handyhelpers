65f6996c8356b8957c565f6c5f2255bf9d74b541
James Perisher <37967469+JamesPerisher@users.noreply.github.com>
2020-04-12 21:01:47 +1000

    killable thread

---
+++ customThreading.py
@@ -0,0 +1,30 @@
+import threading
+import ctypes
+import time
+
+class killableThread(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        pass
+
+    def end(self):
+        pass
+
+
+    def get_id(self):
+        # returns id of the respective thread
+        if hasattr(self, '_thread_id'):
+            return self._thread_id
+        for id, thread in threading._active.items():
+            if thread is self:
+                return id
+
+    def kill(self):
+        thread_id = self.get_id()
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
+              ctypes.py_object(SystemExit))
+        if res > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            print('Exception raise failure')
