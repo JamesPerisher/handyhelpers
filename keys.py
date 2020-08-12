5eed52e256571de5a4c65615f89efb5d36d0ca06
JamesPerisher <37967469+JamesPerisher@users.noreply.github.com>
2020-08-12 09:14:14 +0000

    symetric key

--- keys.py
+++ keys.py
@@ -1,4 +1,5 @@
 import base64
+from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
@@ -7,28 +8,55 @@ from cryptography.hazmat.primitives.asymmetric import padding
 
 
 
+class SymetricKey:
+    def __init__(self, key):
+        self.raw = key
+        self.key = Fernet(key)
+
+    def __repr__(self):
+        return "AsymetricKey(%s)"%self.serialise()
+
+    @classmethod
+    def from_load(cls, data):
+        return cls(data)
+
+    def serialise(self):
+        return self.raw
+
+    @classmethod
+    def from_generate(cls):
+        return cls(Fernet.generate_key())
+
+    def encrypt(self, data):
+        return self.key.encrypt(data.encode()).decode()
+
+    def decrypt(self, data):
+        return self.key.decrypt(data.encode()).decode()
+
+
+
 class AsymetricKey():
     def __init__(self, key):
         self.key = key
 
     @staticmethod
-    def from_pem(key_value, password="a"):
+    def from_pem(cls, key_value, password="a"):
         private_key = serialization.load_pem_private_key(
             key_value.encode(),
             password=password.encode(),
             backend=default_backend()
             )
-        return AsymetricKey(private_key)
+        return cls(private_key)
 
     @staticmethod
-    def from_generate():
+    def from_generate(cls):
         private_key = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048,
             backend=default_backend()
         )
 
-        return AsymetricKey(private_key)
+        return cls(private_key)
 
     def to_pem(self, password="a"):
         pem = self.key.private_bytes(
