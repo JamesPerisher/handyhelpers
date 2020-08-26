ff97871128134e2c54c3c34d7452c0a4b4fa7f13
JamesPerisher <37967469+JamesPerisher@users.noreply.github.com>
2020-08-26 08:42:43 +0000

    timout thread for "with" statements, custom exception

--- customthreading.py
+++ customthreading.py
@@ -3,10 +3,15 @@ import ctypes
 import time
 
 
-class KillableThread(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
+class ThreadExit(Exception):
+    pass
+
+class ThreadExitFailure(Exception):
+    pass
+
+
+class KillableThread(threading.Thread):
     def __enter__(self):
         self.start()
         return self
@@ -32,7 +37,7 @@ class KillableThread(threading.Thread):
               ctypes.py_object(SystemExit))
         if res > 1:
             ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
-            raise Exception('Exception raise failure')
+            raise ThreadExitFailure('Failed to raise exception in thread %s can not kill execution.'%thread_id)
         return self.__exit__()
 
 
@@ -58,3 +63,44 @@ class TimeLoopedThread(KillableThread):
         self.runloop = self.run
         self.run = self._run
         super().start()
+
+
+
+class TimeoutThread(KillableThread):
+    def __init__(self, timeout=1):
+        super().__init__()
+        self.timeout = timeout
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type=None, value=None, traceback=None):
+        if type == ThreadExit : return True
+        super().__exit__()
+
+    def start(self):
+        self.thread_id = threading.current_thread().ident
+        print(dir(self.thread_id))
+        print(self.thread_id)
+        super().start()
+        return self
+
+    def run(self):
+        time.sleep(self.timeout)
+
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(self.thread_id, ctypes.py_object(ThreadExit))
+        if res > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(self.thread_id, 0)
+            raise ThreadExitFailure('Failed to raise exception in thread %s can not kill execution.'%self.thread_id)
+
+
+
+
+
+with limmitedthread(1).start() as a:
+    print(a)
+    while True:
+        print("a")
+        time.sleep(0.1)
+
+print("exited fine")
